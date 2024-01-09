d7f6ecd303ef87abcec579b53f50d38e374cb4a6
Shilpa Reji <shilpa.reji@treeswise.com>
2024-01-09 16:35:08 +0530

    trend and supplier component

--- src/modules/analysis/components/IndexHome.js
+++ src/modules/analysis/components/IndexHome.js
@@ -6,6 +6,8 @@ import { useDispatch } from "react-redux";
 import { actions as commonSliceActions } from "../../common/slice";
 import { actions as sliceAction } from "../slice";
 import OverLaySpend from "./spend/OverLaySpend";
+import OverLayTrend from "./trend/OverLayTrend";
+import OverLaySupplier from "./supplier/OverLaySupplier";
 
 const boxStyle = {
     border: "1px solid black", borderRadius: "15px", display: "flex",
@@ -33,22 +35,28 @@ const IndexHome = () => {
         return () => dispatch(sliceAction.clearAll());
     }, []);
     const handleSpendAnalysis = () => dispatch(sliceAction.setOpenSpend(true));
+    const handleTrendAnalysis = () => dispatch(sliceAction.setOpenTrend(true));
+    const handleSupplierEvaluation = () => dispatch(sliceAction.setOpenSupplier(true));
+
+
     return <Box sx={{ my: 2, overFlowY: "scroll", px: 5, mx: 1 }}>
         <Grid container columnSpacing={3} sx={{ display: "flex", justifyContent: "center", my: 3, alignItems: "center", px: 2 }}>
             <Grid item xs={6} sm={6} md={4} lg={4} xl={4}>
                 <BoxComponent title="Spend Analysis" onClick={handleSpendAnalysis} />
             </Grid>
             <Grid item xs={6} sm={6} md={4} lg={4} xl={4}>
-                <BoxComponent title="Trend Analysis" onClick={handleSpendAnalysis} />
+                <BoxComponent title="Trend Analysis" onClick={handleTrendAnalysis} />
             </Grid>
             <Grid item xs={6} sm={6} md={4} lg={4} xl={4}>
-                <BoxComponent title="Supplier Evaluation" onClick={handleSpendAnalysis} />
+                <BoxComponent title="Supplier Evaluation" onClick={handleSupplierEvaluation} />
             </Grid>
             <Grid item xs={12} sm={12} md={12} lg={11} xl={11}>
                 <RecommendationList />
             </Grid>
         </Grid>
         <OverLaySpend />
+        <OverLayTrend />
+        <OverLaySupplier />
     </Box >;
 
 };
--- src/modules/analysis/selectors.js
+++ src/modules/analysis/selectors.js
@@ -11,3 +11,9 @@ const spendAnalysisForm = (state) => state.spendAnalysisForm;
 export const getSpendAnalysisForm = flow(getState, spendAnalysisForm);
 
 
+const trendAnalysisForm = (state) => state.trendAnalysisForm;
+export const gettrendAnalysisForm = flow(getState, trendAnalysisForm);
+
+const supplierAnalysisForm = (state) => state.supplierAnalysisForm;
+export const getSupplierAnalysisForm = flow(getState, supplierAnalysisForm);
+
