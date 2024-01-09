f918046dcfbe6dd2c4f7dfc7acbfb0617451a64c
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2024-01-09 18:01:20 +0530

    redirection to analysis added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useEffect } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { Typography } from "@mui/material";
 import "./App.css";
 import DashboardLayout from "./layouts/DashboardLayout";
 
 function App() {
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     document.title = ` ${process.env.REACT_APP_PROJECT}`;
+    if (location.pathname === "/") {
+      navigate("/analysis");
+    }
   }, [location]);
   return (
     <Typography className="app" component="div">
